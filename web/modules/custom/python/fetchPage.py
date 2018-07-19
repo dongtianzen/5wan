1f4f9a7eac4e7144f3ec334633e0316c7072dec1
dongtianzen <dongtianzen@gmail.com>
2018-07-19 17:58:29 -0400

    dev convertWebSourceToList

--- web/modules/custom/python/fetchPage.py
+++ web/modules/custom/python/fetchPage.py
@@ -4,23 +4,38 @@ python3 web/modules/custom/python/fetchPage.py
 """
 
 import requests
-import re
 from bs4 import BeautifulSoup
+import re
 
 
 # @param encode = "utf-8"
+# print(requestObj.url)
+# print(requestObj.text)
+#
 def getWebSource(url, encode="gb2312"):
   requestObj = requests.get(url)
   requestObj.encoding = encode
 
   return requestObj
-  return requestObj.text
 
-url = 'http://odds.500.com/fenxi/ouzhi-736656.shtml'
+
+#
+def convertWebSourceToList(requestObj):
+  requestObjText = requestObj.text
+
+  soup = BeautifulSoup(requestObj.text, "html.parser")
+
+  pageTitle = soup.title.text
+  print(pageTitle)
+  return pageTitle
+
+
+url = 'http://odds.500.com/fenxi/ouzhi-523656.shtml'
 requestObj = getWebSource(url)
 
-print(requestObj.url)
-print(requestObj.text)
+convertWebSourceToList(requestObj)
+
+
 
 
 
