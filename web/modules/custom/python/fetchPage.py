378c02a2f044c43e676f72f4f478155e5902c052
dongtianzen <dongtianzen@gmail.com>
2018-07-20 20:05:38 -0400

    dev getCompanyList

--- web/modules/custom/python/fetchPage.py
+++ web/modules/custom/python/fetchPage.py
@@ -21,7 +21,7 @@ class RunGetHistData:
   def getWebSourceObject(self, encode = "gb2312"):
     requestObj = requests.get(self.url)
     requestObj.encoding = encode
-
+    print(24)
     return requestObj
 
   #
@@ -41,12 +41,12 @@ class RunGetHistData:
 
     return soup
 
-  #
+  # @return string, "图卢兹VS雷恩(2015/2016法甲)-百家欧赔-500彩票网"
   def getPageTitle(self):
     pageTitle = self.soup.title.text
     return pageTitle
 
-  #
+  # @return string like "比赛时间2016-02-28 03:00"
   def getGameTime(self):
     gameTimeHtml = self.soup.find(name = "p", attrs = {"class": "game_time"})
     gameTime = gameTimeHtml.string
@@ -56,7 +56,7 @@ class RunGetHistData:
 
     return gameTime
 
-  #
+  # @return string like "3:2"
   def getGameResult(self):
     gameResultHtml = self.soup.find(name = "p", attrs = {"class": "odds_hd_bf"})
     gameResult = gameResultHtml.string
@@ -64,12 +64,27 @@ class RunGetHistData:
     return gameResult
 
   #
+  def getCompanyList(self):
+    companyHtmlArray = self.soup.findAll(name = "span", attrs = {"class": "quancheng"})
+
+    companyList = []
+    for row in companyHtmlArray:
+      companyName = row.string
+      companyName = companyName.strip()     # filter space
+
+      companyList.append(companyName)
+
+    print(companyList)
+
+    return
+
+  #
   def getGameOddList(self):
     gameOddHtmlArray = self.soup.findAll(name = "td", attrs = {"onclick": "OZ.r(this)"})
 
     gameOddList = []
     for row in gameOddHtmlArray:
-      oddValue = row.string.strip()
+      oddValue = row.string
       oddValue = oddValue.strip()     # filter space
 
       gameOddList.append(oddValue)
@@ -81,9 +96,13 @@ class RunGetHistData:
 
 url = 'http://odds.500.com/fenxi/ouzhi-523156.shtml'
 
-pageTitle = RunGetHistData(url).getPageTitle()
+gameObj = RunGetHistData(url)
+pageTitle = gameObj.getPageTitle()
+gameTime  = gameObj.getGameTime()
 
 print(pageTitle)
+print(gameTime)
+print(gameObj.getCompanyList())
 
 
 exit()
