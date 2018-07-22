05fe3b67f0d6a9c217f511775fa0726f3ec6012e
dongtianzen <dongtianzen@gmail.com>
2018-07-22 15:28:50 -0400

    dev FetchPageBasic

--- web/modules/custom/python/FetchPageClass.py
+++ web/modules/custom/python/FetchPageClass.py
@@ -169,37 +169,10 @@ class FetchPageBasic:
 
     output['date'] = self.obtainGameDate()
 
-
-    # output['pageTitle'] = self.soupPageTitle()
-
-    print(output)
-
     return output
 
   # --->
 
-# soupPageTitle, soupGameTime, soupGameResult, use this url
-url = 'http://odds.500.com/fenxi/ouzhi-520156.shtml'
-url = 'http://odds.500.com/fenxi/ouzhi-741189.shtml'
-
-# findGameOddList and findCompanyList use this url
-# url = 'http://odds.500.com/fenxi1/ouzhi.php?id=523156&style=1&start=1&last=1'
-
-
-gameObj = FetchPageBasic(url)
-
-# gameObj.convertPageResultDict()
-
-cc = gameObj.convertPageResultDict()
-
-exit()
-
-
-
-gameObj.findCompanyList()
-gameObj.findGameOddList()
-
-gameObj.getWebSourceText()
 
 
 
--- web/modules/custom/python/fetch5Com.py
+++ web/modules/custom/python/fetch5Com.py
@@ -3,13 +3,42 @@ python3 web/modules/custom/python/fetch5Com.py
 
 """
 
-# from FetchPageClass import FetchPageBasic
+from FetchPageClass import FetchPageBasic
 
 def generateUrl(id):
   url = 'http://odds.500.com/fenxi/ouzhi-' + str(id) + '.shtml'
 
   return url
 
-for num in range(2, 20):
+for num in range(520000, 520004):
+  print(num)
   url = generateUrl(num)
-  print(url)
+
+  gameObj = FetchPageBasic(url)
+  cc = gameObj.convertPageResultDict()
+  print(cc)
+
+exit()
+
+
+exit()
+# soupPageTitle, soupGameTime, soupGameResult, use this url
+url = 'http://odds.500.com/fenxi/ouzhi-520156.shtml'
+url = 'http://odds.500.com/fenxi/ouzhi-741189.shtml'
+
+# findGameOddList and findCompanyList use this url
+# url = 'http://odds.500.com/fenxi1/ouzhi.php?id=523156&style=1&start=1&last=1'
+
+
+gameObj = FetchPageBasic(url)
+
+cc = gameObj.convertPageResultDict()
+
+exit()
+
+
+
+gameObj.findCompanyList()
+gameObj.findGameOddList()
+
+gameObj.getWebSourceText()
