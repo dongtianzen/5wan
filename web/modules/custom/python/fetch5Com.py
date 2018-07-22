59083090201cf817e7e676c6756af94d050a658f
dongtianzen <dongtianzen@gmail.com>
2018-07-22 14:28:35 -0400

    dev generateUrl

--- web/modules/custom/python/fetch5Com.py
+++ web/modules/custom/python/fetch5Com.py
@@ -3,4 +3,13 @@ python3 web/modules/custom/python/fetch5Com.py
 
 """
 
-from FetchPageClass import FetchPageBasic
+# from FetchPageClass import FetchPageBasic
+
+def generateUrl(id):
+  url = 'http://odds.500.com/fenxi/ouzhi-' + str(id) + '.shtml'
+
+  return url
+
+for num in range(2, 20):
+  url = generateUrl(num)
+  print(url)
