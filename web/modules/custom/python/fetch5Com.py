ea03342c4364fa9491630a6fc043f40cb3619e8e
dongtianzen <dongtianzen@gmail.com>
2018-07-22 15:47:59 -0400

    dev BaseSiteClass

--- web/modules/custom/python/fetch5Com.py
+++ web/modules/custom/python/fetch5Com.py
@@ -4,16 +4,11 @@ python3 web/modules/custom/python/fetch5Com.py
 """
 
 from FetchPageClass import FetchPageBasic
-
-def generateUrl(id):
-  url = 'http://odds.500.com/fenxi/ouzhi-' + str(id) + '.shtml'
-
-  return url
+from BaseSiteClass import Baseinfo
 
 for num in range(520000, 520004):
-  print(num)
-  url = generateUrl(num)
-
+  url = Baseinfo().generateUrl(num)
+  print(url)
   gameObj = FetchPageBasic(url)
   cc = gameObj.convertPageResultDict()
   print(cc)
@@ -22,20 +17,12 @@ exit()
 
 
 exit()
-# soupPageTitle, soupGameTime, soupGameResult, use this url
-url = 'http://odds.500.com/fenxi/ouzhi-520156.shtml'
-url = 'http://odds.500.com/fenxi/ouzhi-741189.shtml'
-
-# findGameOddList and findCompanyList use this url
-# url = 'http://odds.500.com/fenxi1/ouzhi.php?id=523156&style=1&start=1&last=1'
-
 
 gameObj = FetchPageBasic(url)
 
 cc = gameObj.convertPageResultDict()
 
 exit()
-
 
 
 gameObj.findCompanyList()
