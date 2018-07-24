e744d27494db106e1ac592814b63b26a75329538
dongtianzen <dongtianzen@gmail.com>
2018-07-24 19:11:26 -0400

    dev queryWinByCondition

--- web/modules/custom/dashpage/src/Content/DashpageContentGenerator.php
+++ web/modules/custom/dashpage/src/Content/DashpageContentGenerator.php
@@ -87,7 +87,8 @@ class DashpageContentGenerator extends ControllerBase {
   public function getTrendContentTbodyRow($section) {
     $output = '';
 
-    $win_nids = $this->queryWinByCondition($code_tid = NULL);
+    $win_nids = $this->queryWinByCondition($ave_win = 2.03);
+    dpm(count($win_nids));
     $win_nodes = \Drupal::entityManager()->getStorage('node')->loadMultiple($win_nids);
 
     foreach ($win_nodes as $key => $win_node) {
@@ -128,23 +129,47 @@ class DashpageContentGenerator extends ControllerBase {
   /**
    *
    */
-  public function queryWinByCondition($ave_win = NULL, $ave_draw = NULL, $ave_loss = NULL, $date = NULL) {
+  public function queryWinByCondition($ave_win = 2.03, $ave_draw = NULL, $ave_loss = NULL, $date = NULL) {
     $query_container = \Drupal::getContainer()->get('flexinfo.querynode.service');
     $query = $query_container->queryNidsByBundle('win');
 
+    $ave_win_diff = 0.001;
     if ($ave_win) {
-      $group = $query_container->groupStandardByFieldValue($query, 'field_win_ave_win', $ave_win);
+      $group = $query_container->groupStandardByFieldValue($query, 'field_win_ave_win', $ave_win - $ave_win_diff, '>');
+      $query->condition($group);
+
+      $group = $query_container->groupStandardByFieldValue($query, 'field_win_ave_win', $ave_win + $ave_win_diff, '<');
       $query->condition($group);
     }
-
-    if ($date) {
-      $group = $query_container->groupStandardByFieldValue($query, 'field_win_date', $date);
+    if ($ave_draw) {
+      $group = $query_container->groupStandardByFieldValue($query, 'field_win_ave_win', $ave_draw);
+      $query->condition($group);
+    }
+    if ($ave_loss) {
+      $group = $query_container->groupStandardByFieldValue($query, 'field_win_ave_win', $ave_loss);
       $query->condition($group);
     }
 
     // $query->sort('field_win_date', 'DESC');
     // $query->range(0, 2);
     $nids = $query_container->runQueryWithGroup($query);
+
+
+    return $nids;
+  }
+
+  /**
+   *
+   */
+  public function queryWinByCompareTime($ave_win = NULL, $ave_draw = NULL, $ave_loss = NULL, $date = NULL) {
+    $query = \Drupal::service('entity.query');
+
+    $query = $query->get('node')
+    ->condition('type', 'win')
+    ->condition('status', 1)
+    ->condition('field_win_ave_win', 2.03, '>');
+
+    $nids = $query->execute();
 
     return $nids;
   }
