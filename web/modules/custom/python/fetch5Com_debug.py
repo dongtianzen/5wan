f17f993a5896c313e1c4cc2e4b6dd7acf5008e09
dongtianzen <dongtianzen@gmail.com>
2018-07-25 21:25:19 -0400

    new fetch5Com_debug.py

--- web/modules/custom/python/fetch5Com_debug.py
+++ web/modules/custom/python/fetch5Com_debug.py
@@ -1,5 +1,5 @@
 """
-python3 web/modules/custom/python/fetch5Com.py
+python3 web/modules/custom/python/fetch5Com_debug.py
 
 """
 import pandas as pd
@@ -12,8 +12,8 @@ from FlexJsonClass import FlexJsonBasic
 
 # start from 520000
 # http://odds.500.com/fenxi/ouzhi-521930.shtml
-startNum = 522000
-endNum   = startNum + 300
+startNum = 521930
+endNum   = startNum + 5
 
 jsonContentDict = {}
 for num in range(startNum, endNum):
@@ -22,9 +22,11 @@ for num in range(startNum, endNum):
   gameObj = FetchPageBasic(url)
   jsonContentDict[num] = gameObj.convertPageResultDict()
 
+# jsonContentDict[666] =  {'ini_win': '2.74', 'ini_draw': '3.20', 'ini_loss': '2.47'}
+
 jsonContent = pd.DataFrame.from_dict(jsonContentDict)
 
-fileName = 'downloadGameInfo.json'
+fileName = 'downloadGameInfo_debug.json'
 jsonFilePath = FlexJsonBasic().getGenerateJsonFilePath(fileName)
 FlexJsonBasic().generateJsonFromData(jsonFilePath, jsonContent)
 
@@ -33,3 +35,4 @@ exit()
 gameObj = FetchPageBasic(url)
 cc = gameObj.convertPageResultDict()
 print(cc)
+
