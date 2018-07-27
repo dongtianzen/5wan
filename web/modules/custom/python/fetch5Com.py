0b33b19bfd0722f3d782719e2a61e104326fab3c
dongtianzen <dongtianzen@gmail.com>
2018-07-26 22:47:19 -0400

    change startNum

--- web/modules/custom/python/fetch5Com.py
+++ web/modules/custom/python/fetch5Com.py
@@ -13,7 +13,7 @@ from FlexJsonClass import FlexJsonBasic
 # start from 520000
 # http://odds.500.com/fenxi/ouzhi-521930.shtml
 startNum = 526000
-startNum = 530889
+startNum = 531415
 endNum   = startNum + 10000
 
 fileName = 'downloadGameInfo.json'
