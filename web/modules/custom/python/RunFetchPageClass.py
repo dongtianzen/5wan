0723558fa7fdf53071244fdee7c1064162c29ed5
dongtianzen <dongtianzen@gmail.com>
2018-07-31 16:02:33 -0400

    dev RunFetchPageBasic

---
+++ web/modules/custom/python/RunFetchPageClass.py
@@ -0,0 +1,43 @@
+"""
+python3 web/modules/custom/python/RunFetchPageClass.py
+
+"""
+
+import pandas as pd
+
+from BaseSiteClass import Baseinfo
+from FetchPageClass import FetchPageBasic
+from FlexJsonClass import FlexJsonBasic
+
+#%%
+#
+class RunFetchPageBasic:
+
+  def runFetch(num):
+    jsonFilePath = FlexJsonBasic().getGenerateJsonFilePath('downloadGameInfo.json')
+
+    url = Baseinfo().generateUrl(num)
+    print(url)
+
+    gameObj = FetchPageBasic(url)
+
+    jsonContentFromFile = FlexJsonBasic().readJsonContent(jsonFilePath)
+
+    try:
+      jsonContentFromFile[num] = gameObj.convertPageResultDict()
+    except:
+      pass
+
+    jsonContent = pd.DataFrame.from_dict(jsonContentFromFile)
+    FlexJsonBasic().generateJsonFromData(jsonFilePath, jsonContent)
+
+    #
+    pageIdJsonContent = { "id" : num}
+
+    pageIdJsonContentDataFrame = pd.DataFrame.from_dict(pageIdJsonContent, orient = 'index')
+    FlexJsonBasic().generateJsonForPageId(pageIdJsonContentDataFrame)
+
+    return
+
+  #%%
+
--- web/modules/custom/python/fetch5Com.py
+++ web/modules/custom/python/fetch5Com.py
@@ -2,40 +2,12 @@
 python3 web/modules/custom/python/fetch5Com.py
 
 """
-import pandas as pd
 
-from BaseSiteClass import Baseinfo
-from FetchPageClass import FetchPageBasic
 from FlexJsonClass import FlexJsonBasic
+from RunFetchPageClass import RunFetchPageBasic
 
 #%%
 #
-jsonFilePath = FlexJsonBasic().getGenerateJsonFilePath('downloadGameInfo.json')
-
-def runFetch(num):
-  jsonContentDict = {}
-
-  url = Baseinfo().generateUrl(num)
-  print(url)
-
-  gameObj = FetchPageBasic(url)
-
-  jsonContentFromFile = FlexJsonBasic().readJsonContent(jsonFilePath)
-  try:
-    jsonContentFromFile[num] = gameObj.convertPageResultDict()
-  except:
-    pass
-
-  jsonContent = pd.DataFrame.from_dict(jsonContentFromFile)
-  FlexJsonBasic().generateJsonFromData(jsonFilePath, jsonContent)
-
-  #
-  pageIdJsonContent = { "id" : num}
-
-  pageIdJsonContentDataFrame = pd.DataFrame.from_dict(pageIdJsonContent, orient = 'index')
-  FlexJsonBasic().generateJsonForPageId(pageIdJsonContentDataFrame)
-
-  return
 
 # start from 520000
 startNum = 526000
@@ -43,11 +15,11 @@ startNum = FlexJsonBasic().getStartPageIdFromJson()
 
 #
 # except Exception: not catch KeyboardInterrupt, SystemExit
-for num in range(startNum, (startNum + 10000)):
+for num in range(startNum, (startNum + 10)):
   try:
-    runFetch(num)
+    RunFetchPageBasic().runFetch(num)
   # except Exception:
-  except:
+  except Exception:
     print("Oops!  That was no valid number.  Try again...")
     pass
 
