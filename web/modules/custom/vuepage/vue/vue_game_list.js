a3d6517ebbb3cd8309c7e4e7ec7eb8b53199a1b3
dongtianzen <dongtianzen@gmail.com>
2018-08-04 22:13:13 -0400

    cc

--- web/modules/custom/vuepage/vue/vue_game_list.js
+++ web/modules/custom/vuepage/vue/vue_game_list.js
@@ -1,31 +1,35 @@
 /**
+ * @to vue-chartjs v3 to draw line chart
+ */
+Vue.component('line-chart', {
+  extends: VueChartJs.Line,
+  mounted () {
+    this.renderChart({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: [40, 39, 10, 40, 39, 80, 40]
+        }
+      ]
+    }, {responsive: true, maintainAspectRatio: false})
+  }
+})
+
+/**
  *
  */
- Vue.component('line-chart', {
-   extends: VueChartJs.Line,
-   mounted () {
-     this.renderChart({
-       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-       datasets: [
-         {
-           label: 'Data One',
-           backgroundColor: '#f87979',
-           data: [40, 39, 10, 40, 39, 80, 40]
-         }
-       ]
-     }, {responsive: true, maintainAspectRatio: false})
-   }
+var vm = new Vue({
+  el: '.appchartjs',
+  data: {
+    message: 'Chart title - Hello World'
+  }
+})
 
- })
-
- var vm = new Vue({
-   el: '.appchartjs',
-   data: {
-     message: 'Hello World'
-   }
- })
-
-
+/**
+ *
+ */
 Vue.component('demo-grid', {
   template: '#grid-template',
   props: {
