7d406f3f250aaa850377a603b6028ea67fc36552
dongtianzen <dongtianzen@gmail.com>
2018-08-06 18:33:41 -0400

    dev VuepageContentGenerator

--- web/modules/custom/vuepage/src/Controller/VuepageController.php
+++ web/modules/custom/vuepage/src/Controller/VuepageController.php
@@ -15,6 +15,9 @@ use Drupal\Core\Url;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
+use Drupal\vuepage\Content\VuepageContentGenerator;
+
+
 
 /**
  * An example controller.
@@ -114,104 +117,15 @@ class VuepageController extends ControllerBase {
 
   /**
    * {@inheritdoc}
-   * @see https://vuejs.org/v2/examples/grid-component.html
-   */
-  public function gameListContent() {
-    $content = NULL;
-    $content .= $this->gameListContentScript();
-    $content .= $this->gameListContentGrid();
-    $content .= $this->gameListContentChartJs();
-
-    return $content;
-  }
-
-  /**
-   *
-   */
-  public function gameListContentChartJs() {
-    $output = '
-      <div class="appchartjs">
-          {{ message }}
-        <line-chart></line-chart>
-      </div>
-    ';
-
-    return $output;
-  }
-
-  /**
-   *
-   */
-  public function gameListContentGrid() {
-    // <!-- <h3 class="vue-title">Grid Component Example - </h3> -->
-    $output = '
-      <div id="game-list-grid-wrapper">
-        <div>
-          <span class="float-right" style="float:right;">
-            Total is {{totalRow}}
-          </span>
-          <form id="search">
-            Search <input name="query" v-model="searchQuery">
-          </form>
-        </div>
-
-        <game-list-grid-tag
-          :data="gridData"
-          :columns="gridColumns"
-          :filter-key="searchQuery"
-        >
-        </game-list-grid-tag>
-      </div>
-    ';
-
-    return $output;
-  }
-
-  /**
-   */
-  public function gameListContentScript() {
-    $output = '
-      <script type="text/x-template" id="grid-template">
-        <table>
-          <thead>
-            <tr>
-              {{filteredTotal}}
-            </tr>
-            <tr>
-              <th v-for="key in columns"
-                @click="sortBy(key)"
-                :class="{ active: sortKey == key }">
-                {{ key | capitalize }}
-                <span class="arrow" :class="sortOrders[key] > 0 ? \'asc\' : \'dsc\'">
-                </span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="entry in filteredData">
-              <td v-for="key in columns">
-                {{entry[key]}}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </script>
-    ';
-
-    return $output;
-  }
-
-  /**
-   * {@inheritdoc}
    */
   public function gameList() {
     $name = 'time_one';
     Timer::start($name);
 
-    $content = $this->gameListContent();
+    $VuepageContentGenerator = new VuepageContentGenerator();
 
     $build = array(
-      '#children' => $content,
+      '#children' => $VuepageContentGenerator->gameListContent(),
       '#attached' => array(
         'library' => array(
           'vuepage/vue',
@@ -227,7 +141,7 @@ class VuepageController extends ControllerBase {
     );
 
     Timer::stop($name);
-    // dpm(Timer::read($name) . 'ms');
+    dpm(Timer::read($name) . 'ms');
 
     return $build;
   }
