d21f04a22f5a566ab8f9433d33d396a7f7f835cc
dongtianzen <dongtianzen@gmail.com>
2018-08-22 18:49:42 -0400

    startNum < 400000

--- web/modules/custom/python/reCheckSuspendAndRun.py
+++ web/modules/custom/python/reCheckSuspendAndRun.py
@@ -18,7 +18,7 @@ diffTimestamp = currentTimestamp - fileLastModifiedTimeStamp
 if(diffTimestamp > 180):
   startNum = FlexJsonBasic().getStartPageIdFromJson()
 
-  if(startNum < 390000):
+  if(startNum < 400000):
 
     # except Exception: not catch KeyboardInterrupt, SystemExit
     for num in range(startNum, (startNum + 1000)):
