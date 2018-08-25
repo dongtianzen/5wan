6f51d1c8aee358325cd7cccf11214b6a3842d4e4
dongtianzen <dongtianzen@gmail.com>
2018-08-24 23:37:01 -0400

    cc

--- web/libraries/webpack.config.js
+++ web/libraries/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: '../modules/custom/vuepage/webpackjs/app.js',
+  entry: '../modules/custom/vuepage/vue/vue_game_list.js',
   output: {
     path: __dirname + '/dist/js',
     filename: 'main_bundle.js'
