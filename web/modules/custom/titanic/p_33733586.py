c5783117a951a82a0d95ad7a3115ffba43c2c907
dongtianzen <dongtianzen@gmail.com>
2018-08-25 18:55:34 -0400

    dev /titanic/p_33733586.py

--- web/modules/custom/titanic/p_33733586.py
+++ web/modules/custom/titanic/p_33733586.py
@@ -8,3 +8,25 @@ Kaggle Titanic 生存预测(Top1.4%)完整代码分享
 https://zhuanlan.zhihu.com/p/33733586
 
 """
+
+# 导入包与加载数据
+
+# %matplotlib inline
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline,make_pipeline
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.feature_selection import SelectKBest
+from sklearn import cross_validation, metrics
+from sklearn.grid_search import GridSearchCV, RandomizedSearchCV
+import warnings
+warnings.filterwarnings('ignore')
+
+train = pd.read_csv('train.csv',dtype={"Age": np.float64})
+test = pd.read_csv('test.csv',dtype={"Age": np.float64})
+PassengerId=test['PassengerId']
+all_data = pd.concat([train, test], ignore_index = True)
