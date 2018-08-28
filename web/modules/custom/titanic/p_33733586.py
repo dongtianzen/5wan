fe721fb6bb6367cd29d6be2f8a5cf680db456243
dongtianzen <dongtianzen@gmail.com>
2018-08-28 07:54:25 -0400

    dev RandomForestClassifier

--- web/modules/custom/titanic/p_33733586.py
+++ web/modules/custom/titanic/p_33733586.py
@@ -21,7 +21,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.feature_selection import SelectKBest
-# from sklearn import cross_validation, metrics
+from sklearn import cross_validation, metrics
 from sklearn.grid_search import GridSearchCV, RandomizedSearchCV
 
 import warnings
@@ -38,9 +38,6 @@ all_data = pd.concat([train, test], ignore_index = True)
 # 1) Sex Feature：女性幸存率远高于男性
 sns.barplot(x="Sex", y="Survived", data=train, palette='Set3')
 
-# 2) Pclass Feature：乘客社会等级越高，幸存率越高
-sns.barplot(x="Pclass", y="Survived", data=train, palette='Set3')
-
 # 5) Age Feature：未成年人幸存率高于成年人
 # 面积图，折线图
 facet = sns.FacetGrid(train, hue="Survived", aspect=2)
@@ -49,18 +46,41 @@ facet.set(xlim=(0, train['Age'].max()))
 facet.add_legend()
 
 # 6) Fare Feature：支出船票费越高幸存率越高
-
 facet = sns.FacetGrid(train, hue="Survived",aspect=2)
 facet.map(sns.kdeplot,'Fare',shade= True)
 facet.set(xlim=(0, 300))
 facet.add_legend()
 
+# 7) Title Feature(New)：不同称呼的乘客幸存率不同
+# 新增Title特征，从姓名中提取乘客的称呼，归纳为六类。
+all_data['Title'] = all_data['Name'].apply(lambda x:x.split(',')[1].split('.')[0].strip())
+Title_Dict = {}
+Title_Dict.update(dict.fromkeys(['Capt', 'Col', 'Major', 'Dr', 'Rev'], 'Officer'))
+Title_Dict.update(dict.fromkeys(['Don', 'Sir', 'the Countess', 'Dona', 'Lady'], 'Royalty'))
+Title_Dict.update(dict.fromkeys(['Mme', 'Ms', 'Mrs'], 'Mrs'))
+Title_Dict.update(dict.fromkeys(['Mlle', 'Miss'], 'Miss'))
+Title_Dict.update(dict.fromkeys(['Mr'], 'Mr'))
+Title_Dict.update(dict.fromkeys(['Master','Jonkheer'], 'Master'))
+all_data['Title'] = all_data['Title'].map(Title_Dict)
+sns.barplot(x="Title", y="Survived", data=all_data, palette='Set3')
 
 # 8) FamilyLabel Feature(New)：家庭人数为2到4的乘客幸存率较高
 # 新创建一个变量
 all_data['FamilySize'] = all_data['SibSp'] + all_data['Parch'] + 1
 
 sns.barplot(x = "FamilySize", y = "Survived", data = all_data, palette = 'Set3')
+
+# 9) Deck Feature(New)：不同甲板的乘客幸存率不同
+# 新增Deck特征，先把Cabin空缺值填充为'Unknown'，再提取Cabin中的首字母构成乘客的甲板号。
+all_data['Cabin'] = all_data['Cabin'].fillna('Unknown')
+all_data['Deck']=all_data['Cabin'].str.get(0)
+sns.barplot(x="Deck", y="Survived", data=all_data, palette='Set3')
+
+# 10) TicketGroup Feature(New)：与2至4人共票号的乘客幸存率较高
+# 新增TicketGroup特征，统计每个乘客的共票号数。
+Ticket_Count = dict(all_data['Ticket'].value_counts())
+all_data['TicketGroup'] = all_data['Ticket'].apply(lambda x:Ticket_Count[x])
+sns.barplot(x='TicketGroup', y='Survived', data=all_data, palette='Set3')
 
 # 再按生存率把FamilySize分为三类，构成FamilyLabel特征
 def Fam_label(s):
@@ -76,28 +96,86 @@ sns.barplot(x="FamilyLabel", y="Survived", data=all_data, palette='Set3')
 
 # plt.show()
 
+
+###
+# 4.数据清洗
+# 1)缺失值填充
+# Age Feature：Age缺失量为263，缺失量较大，用Sex, Title, Pclass三个特征构建随机森林模型，填充年龄缺失值。
+age_df = all_data[['Age', 'Pclass', 'Sex', 'Title']]
+age_df = pd.get_dummies(age_df)
+
+known_age = age_df[age_df.Age.notnull()].as_matrix()
+unknown_age = age_df[age_df.Age.isnull()].as_matrix()
+
+X = known_age[:, 1:]
+y = known_age[:, 0]
+rfr = RandomForestRegressor(random_state=0, n_estimators=100, n_jobs=-1)
+rfr.fit(X, y)
+
+predictedAges = rfr.predict(unknown_age[:, 1::])
+all_data.loc[ (all_data.Age.isnull()), 'Age' ] = predictedAges
+
+# Embarked Feature：Embarked缺失量为2，缺失Embarked信息的乘客的Pclass均为1，且Fare均为80，
+# 因为Embarked为C且Pclass为1的乘客的Fare中位数为80，所以缺失值填充为C。
+all_data[all_data['Embarked'].isnull()]
+
+# sns.boxplot(x="Embarked", y="Fare", hue="Pclass",data=all_data, palette="Set3")
+
+all_data['Embarked'] = all_data['Embarked'].fillna('C')
+
+# Fare Feature：Fare缺失量为1，缺失Fare信息的乘客的Embarked为S，Pclass为3，所以用Embarked为S，Pclass为3的乘客的Fare中位数填充。
+
+all_data[all_data['Fare'].isnull()]
+
+fare=all_data[(all_data['Embarked'] == "S") & (all_data['Pclass'] == 3)].Fare.median()
+all_data['Fare']=all_data['Fare'].fillna(fare)
+
+# 2)同组识别
+# 把姓氏相同的乘客划分为同一组，从人数大于一的组中分别提取出每组的妇女儿童和成年男性。
+
+all_data['Surname']=all_data['Name'].apply(lambda x:x.split(',')[0].strip())
+Surname_Count = dict(all_data['Surname'].value_counts())
+all_data['FamilyGroup'] = all_data['Surname'].apply(lambda x:Surname_Count[x])
+Female_Child_Group=all_data.loc[(all_data['FamilyGroup']>=2) & ((all_data['Age']<=12) | (all_data['Sex']=='female'))]
+Male_Adult_Group=all_data.loc[(all_data['FamilyGroup']>=2) & (all_data['Age']>12) & (all_data['Sex']=='male')]
+
+# 发现绝大部分女性和儿童组的平均存活率都为1或0，即同组的女性和儿童要么全部幸存，要么全部遇难。
+
+Female_Child=pd.DataFrame(Female_Child_Group.groupby('Surname')['Survived'].mean().value_counts())
+Female_Child.columns=['GroupCount']
+Female_Child
+
+sns.barplot(x=Female_Child.index, y=Female_Child["GroupCount"], palette='Set3').set_xlabel('AverageSurvived')
+
+# 绝大部分成年男性组的平均存活率也为1或0。
+
+Male_Adult=pd.DataFrame(Male_Adult_Group.groupby('Surname')['Survived'].mean().value_counts())
+Male_Adult.columns=['GroupCount']
+Male_Adult
+
+sns.barplot(x=Male_Adult.index, y=Male_Adult['GroupCount'], palette='Set3').set_xlabel('AverageSurvived')
+
 # 因为普遍规律是女性和儿童幸存率高，成年男性幸存较低，所以我们把不符合普遍规律的反常组选出来单独处理。
 # 把女性和儿童组中幸存率为0的组设置为遇难组，把成年男性组中存活率为1的设置为幸存组，
 # 推测处于遇难组的女性和儿童幸存的可能性较低，处于幸存组的成年男性幸存的可能性较高。
-
 Female_Child_Group = Female_Child_Group.groupby('Surname')['Survived'].mean()
 Dead_List = set(Female_Child_Group[Female_Child_Group.apply(lambda x:x == 0)].index)
-print(Dead_List)
+# print(Dead_List)
 
 Male_Adult_List = Male_Adult_Group.groupby('Surname')['Survived'].mean()
 Survived_List = set(Male_Adult_List[Male_Adult_List.apply(lambda x:x == 1)].index)
-print(Survived_List)
+# print(Survived_List)
 
 # 为了使处于这两种反常组中的样本能够被正确分类，对测试集中处于反常组中的样本的Age，Title，Sex进行惩罚修改。
 
 train=all_data.loc[all_data['Survived'].notnull()]
 test=all_data.loc[all_data['Survived'].isnull()]
-test.loc[(test['Surname'].apply(lambda x:x in Dead_List)),'Sex'] = 'male'
-test.loc[(test['Surname'].apply(lambda x:x in Dead_List)),'Age'] = 60
-test.loc[(test['Surname'].apply(lambda x:x in Dead_List)),'Title'] = 'Mr'
-test.loc[(test['Surname'].apply(lambda x:x in Survived_List)),'Sex'] = 'female'
-test.loc[(test['Surname'].apply(lambda x:x in Survived_List)),'Age'] = 5
-test.loc[(test['Surname'].apply(lambda x:x in Survived_List)),'Title'] = 'Miss'
+test.loc[(test['Surname'].apply(lambda x:x in Dead_List)), 'Sex'] = 'male'
+test.loc[(test['Surname'].apply(lambda x:x in Dead_List)), 'Age'] = 60
+test.loc[(test['Surname'].apply(lambda x:x in Dead_List)), 'Title'] = 'Mr'
+test.loc[(test['Surname'].apply(lambda x:x in Survived_List)), 'Sex'] = 'female'
+test.loc[(test['Surname'].apply(lambda x:x in Survived_List)), 'Age'] = 5
+test.loc[(test['Surname'].apply(lambda x:x in Survived_List)), 'Title'] = 'Miss'
 
 # 3) 特征转换
 # 选取特征，转换为数值变量，划分训练集和测试集。
@@ -105,8 +183,10 @@ test.loc[(test['Surname'].apply(lambda x:x in Survived_List)),'Title'] = 'Miss'
 all_data = pd.concat([train, test])
 all_data = all_data[['Survived','Pclass','Sex','Age','Fare','Embarked','Title','FamilyLabel','Deck','TicketGroup']]
 all_data = pd.get_dummies(all_data)
+
 train = all_data[all_data['Survived'].notnull()]
-test = all_data[all_data['Survived'].isnull()].drop('Survived',axis=1)
+test = all_data[all_data['Survived'].isnull()].drop('Survived', axis=1)
+
 X = train.as_matrix()[:,1:]
 y = train.as_matrix()[:,0]
 
@@ -115,12 +195,38 @@ y = train.as_matrix()[:,0]
 #
 # 用网格搜索自动化选取最优参数，事实上我用网格搜索得到的最优参数是n_estimators = 28，max_depth = 6。
 # 但是参考另一篇Kernel把参数改为n_estimators = 26，max_depth = 6之后交叉验证分数和kaggle评分都有略微提升。
-pipe = Pipeline([('select',SelectKBest(k=20)),
-               ('classify', RandomForestClassifier(random_state = 10, max_features = 'sqrt'))])
+pipe = Pipeline([
+  ('select',SelectKBest(k=20)),
+  ('classify', RandomForestClassifier(random_state = 10, max_features = 'sqrt'))
+])
 
 param_test = {'classify__n_estimators':list(range(20,50,2)),
               'classify__max_depth':list(range(3,60,3))}
-gsearch = GridSearchCV(estimator = pipe, param_grid = param_test, scoring='roc_auc', cv=10)
+gsearch = GridSearchCV(estimator = pipe, param_grid = param_test, scoring = 'roc_auc', cv = 10)
+
 gsearch.fit(X, y)
 print(gsearch.best_params_, gsearch.best_score_)
+
+# 2)训练模型
+
+select = SelectKBest(k = 20)
+clf = RandomForestClassifier(random_state = 10, warm_start = True,
+                                  n_estimators = 26,
+                                  max_depth = 6,
+                                  max_features = 'sqrt')
+pipeline = make_pipeline(select, clf)
+pipeline.fit(X, y)
+
+# 3)交叉验证
+
+cv_score = cross_validation.cross_val_score(pipeline, X, y, cv= 10)
+print("CV Score : Mean - %.7g | Std - %.7g " % (np.mean(cv_score), np.std(cv_score)))
+
+# 6.预测
+predictions = pipeline.predict(test)
+submission = pd.DataFrame({"PassengerId": PassengerId, "Survived": predictions.astype(np.int32)})
+submission.to_csv("web/modules/custom/titanic/submission.csv", index=False)
+
+exit()
+
 
