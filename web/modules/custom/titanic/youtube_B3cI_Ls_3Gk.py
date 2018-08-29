f0dfd51a7ffbe7a4b3649d8ddb6609b87fe575b9
dongtianzen <dongtianzen@gmail.com>
2018-08-29 16:56:23 -0400

    add comments

--- web/modules/custom/titanic/youtube_B3cI_Ls_3Gk.py
+++ web/modules/custom/titanic/youtube_B3cI_Ls_3Gk.py
@@ -11,24 +11,27 @@ https://www.youtube.com/watch?v=B3cI_Ls_3Gk
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-# 观察前几行的源数据：
+# 导入
 trainData = pd.read_csv('/Applications/MAMP/htdocs/5wan/web/modules/custom/titanic/src/train.csv')
 
+# 观察源数据：
 print(trainData.shape)
 print(trainData.count())
 
+# 1）观察Survived
 trainSurvivedCount = trainData.Survived.value_counts()
 print(trainSurvivedCount)
 
+## 绘图
 fig = plt.figure(figsize = (18, 6))
+
+plt.subplot2grid((2, 3), (0, 0))
 trainSurvivedCount.plot(kind = "bar", alpha = 0.5)
 plt.title("Survived Number")
-plt.show()
 
+plt.subplot2grid((2, 3), (0, 1))
 plt.scatter(trainData.Survived, trainData.Age, alpha = 0.1)
 plt.title("Age vs Survived")
-
 plt.show()
 
 
