986972bfee75cf927049c67703a738f958feb155
dongtianzen <dongtianzen@gmail.com>
2018-09-04 13:14:54 -0400

    dev Train Data Head Teaser

--- web/modules/custom/titanic/5wan_game.py
+++ web/modules/custom/titanic/5wan_game.py
@@ -10,6 +10,8 @@ import pandas as pd
 import seaborn as sns
 import urllib.request
 
+import matplotlib.pyplot as plt
+
 from pandas.io.json import json_normalize
 
 
@@ -21,22 +23,26 @@ def readJsonDecode(urlPath):
   return output
 
 
-# 读入数据, 将json串解析为DataFrame
+### 1) 读入数据, 将json串解析为DataFrame
 pathUrl = 'http://localhost:8888/5wan/web/dashpage/game/list/json?ave_win=7.76'
 trainData = readJsonDecode(pathUrl)
 trainDf = df = json_normalize(trainData['gridData'])
 
-print(trainDf)
-# print(trainData.shape)
-exit()
+### 2) 数据信息总览：
+print("# Train Data Info")
+trainDf.info()
+print("")
 
-# 观察前几行的源数据：
+
+## 观察前几行的源数据：
 # sns.set_style('whitegrid')
-print("# Train Data Head Teaser")
+# print("# Train Data Head Teaser")
 # print(trainData.head())
 
+### 3)
+trainDf['Result'].value_counts().plot.pie(autopct = '%1.2f%%')
 
-# 数据信息总览：
-print("")
-print("# Train Data Info")
-# trainData.info()
+trainDf[['Result','Tags']].groupby(['Result']).mean().plot.bar()
+plt.show()
+
+exit()
