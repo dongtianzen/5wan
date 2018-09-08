d10785c224c659a5b225c2a91e428f36fc95ba1c
dongtianzen <dongtianzen@gmail.com>
2018-09-08 09:43:00 -0400

    dev 5wan_game_train.json

--- web/modules/custom/titanic/5wan_game.py
+++ web/modules/custom/titanic/5wan_game.py
@@ -25,6 +25,7 @@ def readJsonDecode(urlPath):
 
 ### 1) 读入数据, 将json串解析为DataFrame
 pathUrl = 'http://localhost:8888/5wan/web/dashpage/game/list/json?ave_win=5.76'
+pathUrl = 'http://localhost:8888/5wan/web/modules/custom/titanic/src/5wan_game_train.json'
 trainData = readJsonDecode(pathUrl)
 trainDf = json_normalize(trainData['gridData'])
 
@@ -35,9 +36,10 @@ print("")
 
 
 ## 观察前几行的源数据：
-# sns.set_style('whitegrid')
-print("# Train Data Head Teaser")
-print(trainDf.head(30))
+## sns.set_style('whitegrid')
+
+# print("# Train Data Head Teaser")
+# print(trainDf.head(30))
 
 ### 3)
 # trainDf['Result'].value_counts().plot.pie(autopct = '%1.2f%%')
@@ -46,14 +48,22 @@ print(trainDf.head(30))
 # plt.show()
 
 
-### 4) 特征间相关性分析 生成特征之间的关联图
+### 4) 特征选择： 相关性矩阵
 correlationDF = pd.DataFrame(trainDf[
   ['Draw', 'Loss', 'Win', 'Result']
 ])
 
+correlationResult = correlationDF.astype(float).corr()
+print(correlationResult)
+print(correlationResult['Result'].sort_values(ascending = False))
+
+## 特征间相关性分析 生成特征之间的关联图
 colormap = plt.cm.viridis
 plt.figure(figsize = (10, 8))
 plt.title('Pearson Correlation of Features', y = 1.05, size = 12)
-sns.heatmap(correlationDF.astype(float).corr(), linewidths = 0.1, vmax = 1.0, square = True, cmap = colormap, linecolor = 'white', annot = True)
+sns.heatmap(correlationResult, linewidths = 0.1, vmax = 1.0, square = True, cmap = colormap, linecolor = 'white', annot = True)
 plt.show()
+
+
+exit()
 
