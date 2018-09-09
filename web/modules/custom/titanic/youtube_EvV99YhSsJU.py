5817af58c440ac4acaa52d5ea32588b4a962fb93
dongtianzen <dongtianzen@gmail.com>
2018-09-09 13:33:27 -0400

    dev SVM

--- web/modules/custom/titanic/youtube_EvV99YhSsJU.py
+++ web/modules/custom/titanic/youtube_EvV99YhSsJU.py
@@ -17,6 +17,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from sklearn import datasets
+from sklearn import svm
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -39,18 +42,37 @@ print(irisTest.shape)
 print(targetTrain.size)
 print(targetTest.size)
 
-#
+## KNN算法， KNeighborsClassifier()
 knn = KNeighborsClassifier()
 knn.fit(irisTrain, targetTrain)
 
 testPredict = knn.predict(irisTest)
-print(testPredict)
-print(targetTest)
+# print(testPredict)
+# print(targetTest)
 
-# width = 0.35
-# fig, ax = plt.subplots()
-# ax.bar(testPredict, targetTest, width, color='r')
-# plt.show()
+## .5 分类报告
+# classification_report函数构建了一个文本报告，用于展示主要的分类
+# 按类别输出 准确率，召回率， F1值--平衡F-score
+print(classification_report(targetTest, testPredict))
+
+## SVM分类器
+clf = svm.SVC(C=0.1, kernel='linear', decision_function_shape='ovr')
+
+# 不同的参数调试
+# clf = svm.SVC(C=0.8, kernel='rbf', gamma=20, decision_function_shape='ovr')
+clf.fit(irisTrain, targetTrain)
+
+# 准确率
+# 精度
+print (clf.score(irisTrain, targetTrain))
+print ('Train set accuracy_score is:', accuracy_score(targetTrain, clf.predict(irisTrain)))
+print (clf.score(irisTest, targetTest))
+print ('Test set accuracy_score is', accuracy_score(targetTest, clf.predict(irisTest)))
+
+# decision_function
+# 计算样本点到分割超平面的函数距离
+# print ('decision_function:\n', clf.decision_function(irisTrain))
+# print ('\npredict:\n', clf.predict(irisTrain))
 
 
 
