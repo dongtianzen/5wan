7ea99ae72203e08ac7e0f3287002246e15da3e60
dongtianzen <dongtianzen@gmail.com>
2018-09-10 22:20:57 -0400

    cc

--- web/modules/custom/titanic/sklearn_game.py
+++ web/modules/custom/titanic/sklearn_game.py
@@ -17,6 +17,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from pandas.io.json import json_normalize
 
+rng = np.random.RandomState(0)
+X = rng.rand(100, 10)
+y = rng.binomial(1, 0.5, 100)
+print(type(X))
+print(type(y))
+
 
 ### def function read json
 def readJsonDecode(urlPath):
@@ -33,15 +39,20 @@ jsonData = readJsonDecode(pathUrl)
 jsonDataDf = json_normalize(jsonData)
 
 gameDF = jsonDataDf[['ave_win', 'ave_draw', 'ave_loss', 'ini_win', 'ini_draw', 'ini_loss']]
-resultDF = jsonDataDf[['Result']]
+resultNdArray = jsonDataDf[['Result']]
 
-X_train, X_test, y_train, y_test = train_test_split(gameDF, resultDF, test_size = 0.3)
+# print("")
+# print("# type(resultNdArray)")
+print(type(resultNdArray))
+#
+X_train, X_test, y_train, y_test = train_test_split(gameDF, resultNdArray, test_size = 0.3)
+
 
 
 ### 2) 数据信息总览：
-print("# Train Data Info")
-jsonDataDf.info()
-print("")
+# print("# Train Data Info")
+# jsonDataDf.info()
+# print("")
 
 ## 观察前几行的源数据：
 # sns.set_style('whitegrid')
@@ -51,12 +62,12 @@ print(y_train.head(10))
 
 ### 1） KNN算法， KNeighborsClassifier()
 knnModel = KNeighborsClassifier()
-knnModel.fit(X_train, y_train)
+knnModel.fit(X_train, y_train.values.reshape(-1,))
 
 y_predict = knnModel.predict(X_test)
 
-# print model
-print(knnModel)
+## print model
+# print(knnModel)
 # print(y_predict)
 # print(y_test)
 
@@ -64,13 +75,13 @@ print(knnModel)
 ## classification_report函数构建了一个文本报告，用于展示主要的分类
 ## 按类别输出 准确率，召回率， F1值--平衡F-score
 
+
 print(classification_report(y_test, y_predict))
-exit()
 
 
 ## new DataFrame
 resultDF = pd.DataFrame()
-resultDF['y_test'] = y_test
+resultDF['y_test'] = y_test.values.reshape(-1,)
 resultDF['Predict'] = y_predict
 ## 对比结果
 resultDF['Result'] = 0
