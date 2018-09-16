a0b13b0716c66b04c79fcd30f7ac7aa4cb2e5846
dongtianzen <dongtianzen@gmail.com>
2018-09-16 09:17:02 -0400

    dev sort_values

--- web/modules/custom/titanic/game_basic.py
+++ web/modules/custom/titanic/game_basic.py
@@ -22,6 +22,8 @@ print("# Train Data Info")
 jsonDataDf.info()
 print("")
 
+## 排序打印Dataframe
+# print(jsonDataDf['Result'].sort_values(ascending = False))
 
 ## 观察前几行的源数据：
 ## 因为读入数据有中文，在sublime text内运行有问题，需要到命令行运行
@@ -35,52 +37,56 @@ print("# 3 1 0 proportion")
 print(jsonDataDf['Result'].value_counts(normalize = True))
 print("")
 
+jsonDataDf['ave_win'] = jsonDataDf['ave_win'].astype(float)
+jsonDataDf['ave_loss'] = jsonDataDf['ave_loss'].astype(float)
+jsonDataDf['Result'] = jsonDataDf['Result'].astype(int)
+
 ## 百分比
 # jsonDataDf['Result'].value_counts(normalize = True).plot(kind = "bar", alpha = 0.5)
 # plt.show()
 
-## 个数分布
-# sns.distplot(jsonDataDf['Result'].astype(int), bins = 6, kde = False)
-# plt.show()
-
-jsonDataDf['ave_win'] = jsonDataDf['ave_win'].astype(float)
-jsonDataDf['ave_loss'] = jsonDataDf['ave_loss'].astype(float)
-
-### 4) 两个特征的关系，两个变量之间的分布关系
-### 散点图
-sns.jointplot(x = 'ave_win', y = 'ave_loss', data = jsonDataDf)
-plt.show()
-
-## 用“hex”来展示出现频率，当数据量比较多的时候，散点堆积重叠
-sns.jointplot(x = 'ave_win', y = 'ave_loss', data = jsonDataDf, kind = "hex", color = "k")
-plt.show()
-
-exit()
-
-### 3) Plot
+## Pie chart
 # jsonDataDf['Result'].value_counts().plot.pie(autopct = '%1.2f%%')
 # plt.show()
 
+## 个数分布
+# sns.distplot(jsonDataDf['Result'], bins = 6, kde = False)
+# plt.show()
+
+
+### 4) 两个特征的关系，两个变量之间的分布关系
+### 散点图
+
+# sns.jointplot(x = 'ave_win', y = 'ave_loss', data = jsonDataDf)
+# plt.show()
+
+## 用“hex”来展示出现频率，当数据量比较多的时候，散点堆积重叠
+# sns.jointplot(x = 'ave_win', y = 'ave_loss', data = jsonDataDf, kind = "hex", color = "k")
+# plt.show()
+
+
+### 3) Pair Plot, 3 x 3 的plot 用来特征两两对比，对角线上是单变量直方图
+# tempDF = pd.DataFrame(jsonDataDf[
+#   ['ave_win', 'ave_loss', 'Result']
+# ])
+# sns.pairplot(tempDF)
+# plt.show()
+
+### 4) 分组图
 # jsonDataDf[['Result', 'Tags']].groupby(['Result']).mean().plot.bar()
 # plt.show()
 
 
-### 4) 特征选择： 相关性矩阵
-correlationDF = pd.DataFrame(jsonDataDf[
-  ['ini_draw', 'ini_loss', 'ini_win', 'Result']
-])
-
-correlationResult = correlationDF.astype(float).corr()
-# print(correlationResult)
-# print(correlationResult['Result'].sort_values(ascending = False))
-
-## 特征间相关性分析 生成特征之间的关联图
+### 5) 特征选择： 相关性矩阵
+# # 特征间相关性分析 生成特征之间的关联图
+# correlationResult = pd.DataFrame(jsonDataDf[
+#   ['ave_win', 'ave_loss', 'Result']
+# ])
 # colormap = plt.cm.viridis
 # plt.figure(figsize = (10, 8))
 # plt.title('Pearson Correlation of Features', y = 1.05, size = 12)
 # sns.heatmap(correlationResult, linewidths = 0.1, vmax = 1.0, square = True, cmap = colormap, linecolor = 'white', annot = True)
 # plt.show()
-
 
 
 exit()
