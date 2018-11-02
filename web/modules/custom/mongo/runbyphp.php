a7531e098ba974be5bf37e7e54d5ef41e5b2d31a
dongtianzen <dongtianzen@gmail.com>
2018-11-01 23:02:52 -0400

    connect MongoDB\Driver\Manager

--- web/modules/custom/mongo/runbyphp.php
+++ web/modules/custom/mongo/runbyphp.php
@@ -9,11 +9,12 @@
 
 function _run_create_fields() {
 
-  $MongoClient = new MongoClient();
+  // $MongoClient = new MongoClient();
 
+  $MongoClient = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 
-  $db = $MongoClient->5wan; //选择test数据库
+  $db = $MongoClient->selectDatabase->5wan; //选择test数据库
 
-  $c = $db->game; //选择一个集合
+  // $c = $db->game; //选择一个集合
 
 }
