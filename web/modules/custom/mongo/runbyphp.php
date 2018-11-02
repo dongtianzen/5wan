e933ea1efc6a0d724ccb44f01a520d1635ca1bd9
dongtianzen <dongtianzen@gmail.com>
2018-11-02 10:18:59 -0400

    dev class MongoDriverManager

--- web/modules/custom/mongo/runbyphp.php
+++ web/modules/custom/mongo/runbyphp.php
@@ -3,7 +3,9 @@
 /**
  *
   require_once(DRUPAL_ROOT . '/modules/custom/mongo/runbyphp.php');
-  _run_create_fields();
+
+  $MongoDriverManager = new MongoDriverManager();
+  $MongoDriverManager->runQueryFieldsWithHideFields();
  */
 
 /**
@@ -11,90 +13,119 @@
  *
  * http://zetcode.com/db/mongodbphp/
  */
-function _run_create_fields() {
 
-  // $MongoClient = new MongoClient();
+/**
+ * An example controller.
+ */
+class MongoDriverManager {
 
-  // $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+  /**
+   *
+   */
+  public $manager;
 
-  $client = new MongoDB\Client('mongodb://localhost:27017');
-  var_dump($client->listDatabases());
+  public function __construct() {
+    $this->manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+
+    // $client = new MongoDB\Client('mongodb://localhost:27017');
+    // var_dump($client->listDatabases());
+  }
 
   /**
    * insert
    */
-  // $bulk = new MongoDB\Driver\BulkWrite;
-  // $bulk->insert(['ave_win' => 6]);
-  // $manager->executeBulkWrite('5wan.game', $bulk);
+  function runInsertFields() {
+    $bulk = new MongoDB\Driver\BulkWrite;
+    $bulk->insert(['ave_win' => 6]);
+
+    $this->manager->executeBulkWrite('5wan.game', $bulk);
+    // $doc = ['_id' => new MongoDB\BSON\ObjectID, 'name' => 'Toyota', 'price' => 26700];
+    // $bulk->insert($doc);
+  }
 
   /**
-   * update and delete
+   * update
    * The MongoDB\BSON\ObjectID generates a new ObjectId. It is a value used to uniquely identify documents in a collection.
    */
-  // $doc = ['_id' => new MongoDB\BSON\ObjectID, 'name' => 'Toyota', 'price' => 26700];
-  // $bulk->insert($doc);
-  // $bulk->update(['name' => 'Audi'], ['$set' => ['price' => 52000]]);
-  // $bulk->delete(['name' => 'Hummer']);
+  function runUpdateFields() {
+    $bulk = new MongoDB\Driver\BulkWrite;
+    $bulk->update(['name' => 'Audi'], ['$set' => ['price' => 52000]]);
+
+    $this->manager->executeBulkWrite('5wan.game', $bulk);
+  }
+
+  /**
+   * delete
+   */
+  function runDeleteFields() {
+    $bulk = new MongoDB\Driver\BulkWrite;
+    $bulk->delete(['name' => 'Hummer']);
+
+    $this->manager->executeBulkWrite('5wan.game', $bulk);
+  }
 
   /**
    * query
    */
-  // $filter = ['ave_win' => ['$gt' => 1]];
-  // $filter = [ 'name' => 'Volkswagen' ];
-  // $filter = [];
-  // $options = [
-  //   'projection' => [
-  //     '_id' => 0,
-  //     'ave_win' => 0,
-  //   ],
-  //   'sort' => ['ave_win' => -1],    // 1 or -1
-  //   'maxTimeMS' => 3000,
-  //   'limit' => 5
-  // ];
-  // $options = [];
+  function runQueryFields() {
+    $filter = ['ave_win' => ['$gt' => 1]];
+    $filter = ['name' => 'Volkswagen'];
+    $filter = [];
+    $options = [
+      'projection' => [
+        '_id' => 0,
+        'ave_win' => 0,
+      ],
+      'sort' => ['ave_win' => -1],    // 1 or -1
+      'maxTimeMS' => 3000,
+      'limit' => 5
+    ];
+    $options = [];
 
-  // $query = new MongoDB\Driver\Query($filter, $options);
-  // $rows = $manager->executeQuery('5wan.game', $query);
+    $query = new MongoDB\Driver\Query($filter, $options);
+    $rows = $this->manager->executeQuery('5wan.game', $query);
 
-  // foreach ($rows as $document) {
-  //   dpm(999);
-  //   dpm($document->ave_win);
-  //   // ksm($document);
-  //   var_dump($document);
-  // }
+    foreach ($rows as $document) {
+      dpm($document->ave_win);
+      // ksm($document);
+      var_dump($document);
+    }
+  }
 
   /**
-   * Database statistics
-   */
-  // $stats = new MongoDB\Driver\Command(["dbstats" => 1]);
-  // $result = $manager->executeCommand("5wan", $stats);
-
-  // $stats = current($result->toArray());
-
-  // print_r($stats);
-
-
-  //
-  /**
+   * query
    * Projections
    * Projections can be used to specify which fields should be returned.
    * Here we hide the '_id' field and 'ave_win' field when the return result.
    */
-  // $options = [
-  //   "projection" => [
-  //     '_id' => 0,
-  //     'ave_win' => 0,
-  //   ]
-  // ];
-  // $filter = [];
-  // $query = new MongoDB\Driver\Query($filter, $options);
+  function runQueryFieldsWithHideFields() {
+    $options = [
+      "projection" => [
+        '_id' => 0,
+        'ave_win' => 0,
+      ]
+    ];
+    $filter = [];
+    $query = new MongoDB\Driver\Query($filter, $options);
 
-  // $rows = $manager->executeQuery("5wan.game", $query);
+    $rows = $this->manager->executeQuery("5wan.game", $query);
 
-  // foreach ($rows as $row) {
+    foreach ($rows as $row) {
+      print_r($row);
+    }
+  }
 
-  //        print_r($row);
-  // }
+  /**
+   * Database statistics
+   */
+  function runDatabaseStats() {
+    $stats = new MongoDB\Driver\Command(["dbstats" => 1]);
+    $result = $this->manager->executeCommand("5wan", $stats);
 
+    $stats = current($result->toArray());
+
+    print_r($stats);
+  }
 
 }
+
