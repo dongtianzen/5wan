3dc6b7eb36666bde8929231e45561bff5968e7d1
dongtianzen <dongtianzen@gmail.com>
2018-12-06 10:52:40 -0500

    dev range(179000

--- web/modules/custom/python/pymongo/runCheckDuplication.py
+++ web/modules/custom/python/pymongo/runCheckDuplication.py
@@ -44,7 +44,7 @@ class BasicQueryPyMongo:
 
     logging.warning("Running warning to")
 
-    for num in range(114000, 201000):
+    for num in range(179000, 201000):
       queryObj = { "id5": num }
 
       if ((num % 1000) == 0):
